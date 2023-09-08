169618f61e23d9219675d89ca34794503eca53e3
yaswanthbandaru <yaswanthbandaru424@gmail.com>
2023-09-08 12:11:19 +0530

    Tailwind as been added

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='text-3xl font-bold underline'>
+        Hello yashwanth!
+      </p>
+    </div>
+  );
+}
+
+export default App;
